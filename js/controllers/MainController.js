56083e2839ed64f6f6eb749604c1cef82684c17e
chooomedia <christopher@chooo.media>
2018-02-25 10:05:31 +0100

    update

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,27 @@
+// app.factory('instaPics', ['$http', function($http){
+// let cid = '986f99861521472fb2aa1a5496968382';
+
+//       return {
+//         fetchInstagram: function() {       
+//             var url = "https://api.instagram.com/v1/users//chooomedia/media/recent?client_id=" + cid + "&callback=JSON_CALLBACK";
+//             return $http.jsonp(url).then(function(response) {
+//                 return response.data;
+//             });
+//         }
+//     }
+// }]);
+
 app.controller('scope-gallery', function($scope, $http) {
+
+// instagramCtrl = this;
+// this.instagramPosts = [];   
+// this.loading = true;
+//   instaPics.fetchInstagram(function (data){
+//     instagramCtrl.loading = false;  
+//     for(var i = 0; i < 5; i++){
+//          scope-gallery.instagramPosts.push(data[i]);
+//     }        
+// });
 
   $scope.status = "Loading...";
   $scope.loading = true;
@@ -26,17 +49,23 @@ app.controller('scope-gallery', function($scope, $http) {
     $scope.fileHref = "http://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";  
   }
 
-//  // load INSTAGRAM Photos
-//   $http({
-//     method: 'GET',
-//     url: 'https://api.instagram.com/v1/media/{media-id}?access_token=5556067055.986f998.fa47ce1bddf34325a129d6d4f99e1586'
-//   }).then(function(response) {
-//     $scope.photos = response;
-//     console.log(response);
+// socialMedia.fetchInstagram().then(function(data) {
+//   $scope.data = data;
+//   console.log($scope.data);
+// });
+
+// let photo_count = 99;
+// let token = '5556067055.986f998.fa47ce1bddf34325a129d6d4f99e1586';
+// let data = {access_token: token, count: photo_count};
+
+// $http.get('https://api.instagram.com/v1/media/self/media/recent&format=json&nojsoncallback=1'')
+//   .then(function(data) {
+//     $scope.content = response.data;
+//     $scope.statuscode = response.status;
+//     $scope.statustext = response.statusText;
 //   });
 
-
-// // load AMAZON Products
+// load AMAZON Products
 //   $http({
 //     method: 'GET',
 //     url: 'http://webservices.amazon.de/onca/json?Service=AWSECommerceService&Operation=ItemSearch&SubscriptionId=AKIAIELN3ERONKG4TMKQ&AssociateTag=mucrum-21&SearchIndex=All&Keywords=smartphone&ResponseGroup=Images,ItemAttributes,Offers'
