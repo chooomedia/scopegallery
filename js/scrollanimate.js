5302b930c0e9a8bec8a8268ad9b331faf9a974f5
Chris Matt <chooom@gmx.de>
2018-11-25 20:06:16 +0100

    added data from choooFtp

--- js/scrollanimate.js
+++ js/scrollanimate.js
@@ -1,22 +1,22 @@
 var mywindow = $(window);
-var mypos = mywindow.scrollTop();
-var up = false;
-var newscroll;
-mywindow.scroll(function () {
-    newscroll = mywindow.scrollTop();
-    if (newscroll > mypos && !up) {
-        $('.panel-rounded, .panel-navbar').stop().slideToggle('fast');
-        up = !up;
-        console.log(up);
-    } else if (newscroll < mypos && up) {
-        $('.panel-rounded, .panel-navbar').stop().slideToggle('fast');
-        up = !up;
-    }
-    mypos = newscroll;
-});
+    var mypos = mywindow.scrollTop();
+    var up = false;
+    var newscroll;
+    mywindow.scroll(function () {
+        newscroll = mywindow.scrollTop();
+        if (newscroll > mypos && !up) {
+            $('.panel-rounded, .panel-navbar').stop().slideToggle('fast');
+            up = !up;
+            console.log(up);
+        } else if(newscroll < mypos && up) {
+            $('.panel-rounded, .panel-navbar').stop().slideToggle('fast');
+            up = !up;
+        }
+        mypos = newscroll;
+    });
 
-//Click event to scroll to top
-$('.scrollToTop').click(function () {
-    $('html, body').animate({scrollTop: 0}, 1400);
-    return false;
-});
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
